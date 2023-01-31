dc2ceb174c66fa956d0a6fe9fbfdc5d5f762ac13
Matheus Araújo <matheus.daf@outlook.com>
2023-01-30 22:23:14 -0300

    :rocket: fix deploy

--- src/context/CycleContext.tsx
+++ src/context/CycleContext.tsx
@@ -61,7 +61,7 @@ export function CyclesContextProvider({ children }: ChildrenInterface) {
     },
   )
 
-  const { cycles, activeCycleId } = cyclesState
+  const { cycles, activeCycleId } = cyclesState || null
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
